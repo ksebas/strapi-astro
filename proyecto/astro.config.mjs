67cfefc7574f082396936101b3f62bccc1bb3535
Sebastian Sanabria <sebsanab2@publicisgroupe.net>
2026-05-25 08:36:57 -0500

    fix: configure astro vercel adapter

--- proyecto/astro.config.mjs
+++ proyecto/astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
-
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+
   vite: {
     plugins: [tailwindcss()],
     resolve: {
